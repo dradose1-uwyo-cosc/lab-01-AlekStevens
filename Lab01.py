734ddf9ce13f8c14c94fece6a6d303be88f2b041
AlekStevens <asteve23@uwyo.edu>
2024-09-17 22:07:41 +0000

    commit

---
+++ Lab01.py
@@ -0,0 +1,16 @@
+# Alek Stevens
+# UWYO COSC 1010
+# 09/10/2024
+# Lab 01
+# Lab section 10
+# Sources: Myself,
+print("hello world")
+x = 5
+y = 6.5
+z = x + y
+print(z)
+first = "Alek"
+last = "Stevens"
+major = "Mechanical Engineer"
+print("My name is:",first,last)
+print("Major:",major)
